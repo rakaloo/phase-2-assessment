0aef926f5c75cec08906f4ea16d8882a5200929a
Rachael Lunney <email": null>
2016-10-19 10:59:02 -0500

    Create user model, migration. Add session, login, logout logic

---
+++ part-3/db/migrate/20161019103101_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :username
+      t.string :hashed_password
+
+      t.timestamps(null: false)
+    end
+    add_index(:users, :username, unique: true)
+    add_index(:users, :email, unique: true)
+  end
+end
