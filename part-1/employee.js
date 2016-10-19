ebed2c39f82de06e04c47a3eba5a7e111f6de328
Rachael Lunney <email": null>
2016-10-19 09:36:38 -0500

    Add employee object and functions

--- part-1/employee.js
+++ part-1/employee.js
@@ -1,1 +1,16 @@
+var Employee = function(object) {
+  this.firstName = object.firstName;
+  this.sales = object.sales;
 
+  this.totalSales = function() {
+    var sum = 0;
+    for (var i = 0; i < this.sales.length; i++) {
+      sum += this.sales[i];
+    }
+    return sum;
+  }
+
+  this.averageSales = function() {
+    return (this.totalSales()/this.sales.length);
+  }
+}
