8c4a1ce9c381a1717d4aa13a9d2712f922b9a3f0
Rachael Lunney <email": null>
2016-10-19 11:56:37 -0500

    Add new restaurant routes, views and logic

--- part-3/app/controllers/restaurants.rb
+++ part-3/app/controllers/restaurants.rb
@@ -1,8 +1,20 @@
 get '/restaurants/new' do
   authenticate!
+  @restaurant = Restaurant.new
   erb :"restaurants/new"
 end
 
 post '/restaurants' do
+  @restaurant = Restaurant.new(params[:restaurant])
+  @restaurant.owner = current_user
+
+  if @restaurant.save
+    redirect "/restaurants/#{@restaurant.id}"
+  else
+    erb :"restaurants/new"
+  end
+end
+
+post 'restaurants/:id' do
 
 end
